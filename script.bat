da1c2306b2c9044e7f77ea19893d6362ee686d0c
KrishnaXavier <tsixav@gmail.com>
2018-06-01 19:50:13 -0300

    Preparando teste para script.bat

---
+++ script.bat
@@ -0,0 +1,23 @@
+REM[source project: https://github.com/KrishnaXavier/auto-hospedagem-github]
+
+:: criando branch orfam
+
+git checkout --orphan gh-pages
+
+checkout gh-pages
+
+
+:: origem do código que vai ser hospedado, caso não seja o master trocar
+git pull origin master
+
+
+
+:: upando as alterações
+
+git add .
+
+git commit -a -m
+
+REM[Informe seus dados de acesso, caso solicitado:]
+
+git push origin gh-pages
